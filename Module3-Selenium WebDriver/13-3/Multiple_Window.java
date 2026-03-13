d0df937d18a1bc67c2522858b6aeda59caceaf5c
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2026-03-13 13:10:38 +0530

    commit

---
+++ Module3-Selenium WebDriver/13-3/Multiple_Window.java
@@ -0,0 +1,28 @@
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Multiple_Window 
+{
+	public static void main(String[] args) 
+	{
+		WebDriver driver = MyConnection.connect("http://demo.guru99.com/popup.php");
+		
+		String mainWin = driver.getWindowHandle();
+		driver.findElement(By.linkText("Click Here")).click();
+		
+		Set<String> allwins =  driver.getWindowHandles();
+		for(String win : allwins)
+		{
+			if(!win.equals(mainWin))
+			{
+				driver.switchTo().window(win);
+			} 
+		}
+		
+		driver.findElement(By.name("emailid")).sendKeys("tops@gmail.com");
+		driver.findElement(By.name("btnLogin")).click();
+		
+	}
+}
---
+++ Module3-Selenium WebDriver/All Revision/util/MyConnection - use this if version is abover 127.java
@@ -0,0 +1,26 @@
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class MyConnection 
+{
+	static WebDriver driver;
+	
+	public static WebDriver connect(String url)
+	{
+		//145
+		//System.setProperty("webdriver.chrome.driver", "F:\\New\\New2\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		
+		//127
+		System.setProperty("webdriver.chrome.driver", "F:\\chrome-127\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
+		//ChromeOptions options = new ChromeOptions();
+		//options.addArguments("--remote-allow-origins=*");
+		driver = new ChromeDriver();
+		driver.get(url);
+		driver.manage().window().maximize();
+		return driver;
+	}
+}
